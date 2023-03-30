46d05433a49272114085263f9ce201c6e329d2a3
Melissa996 <melissacusumano96@gmail.com>
2023-03-30 16:34:37 +0200

    aggiunta esercizi basic 2

---
+++ es_7_convert values/exercise.js
@@ -0,0 +1,10 @@
+let hello = 'Ciao';     // number
+let age = 18;       // boolean
+let isGraduated = false;     // string
+
+console.log (Number(hello))
+console.log (Boolean(age))
+console.log (String(isGraduated))
+
+// let hello = 'Ciao'; --> non è possibile convertire una stringa in una stringa di numeri, 
+//il risultato sarà Nun (perchè state considerando qualcosa che non dovrebbe essere un numero)
---
+++ es_8_convert values/exercise.js
@@ -0,0 +1,17 @@
+let Var = true;
+//console.log(Var) //stampa il suo valore
+//console.log(typeof(Var)) //stampa il tipo di dato (boolean)
+
+
+ Var = Number(Var);
+//console.log(Var)
+//console.log(typeof(Var)) // il numero booleano, in questo caso 1 perchè la var è true
+
+
+ Var = String(Var);
+//console.log(Var)
+//console.log(typeof(Var))
+
+ Var = Boolean(Var)
+//console.log(Var)
+//console.log(typeof(Var))
